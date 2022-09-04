bdf260619fe18221279cff2abf501cac1bda3c65
Sam Sarsten <sam.sarsten@gmail.com>
2022-09-04 14:37:02 -0700

    Post-addition commit
    Appears to be working properly.
    Need to prepare for 'equals' button press with this.operator variable.

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 const calculator = {
-    keyPress: '',
-    operands: [undefined, undefined],
+    keyPress: 'a',
+    operands: ['a', 'a'],
     currentOperand: 0,
     numString: '',
     //intString: '',
     //decString: '',
     //float: false,
     negative: false,
+    screen: '',
 
     getOperand(index) {
         return this.operands[index];
@@ -42,10 +43,61 @@ const calculator = {
     },
 
     handleKeyPress() {
-        if (!isNaN(this.keyPress)) {
-            this.numString += this.keyPress.toString();
+        const kp = this.keyPress;
+        if (!isNaN(kp)) {
+            if (this.numString.length < 10) {
+                this.numString += kp.toString();
+                //this.screen = this.numString;
+                this.updateScreen(this.numString);
+            }
+        } else if (kp === "+") {
+            calculator.addition();
+        } else if (kp === "-") {
+            calculator.subtraction();
+        } else if (kp === "*") {
+            calculator.multiplication();
+        } else if (kp === "÷") {
+            calculator.division();
+        } else if (kp === "%") {
+            calculator.percent();
+        } else if (kp === "B") {
+            calculator.backspace();
+        } else if (kp === "C") {
+            calculator.clear();
+        } else if (kp === "=") {
+            calculator.evaluate();
         }
+    },
+
+    addition() {
+        if (this.numString.length > 0) {
+            this.operator = "+";
+            this.operands[this.currentOperand] = Number(this.numString);
+            console.log(this.operands);
+            if (this.currentOperand === 1) {
+                this.operands[0] = this.operands[0] + this.operands[1];
+                this.operands[1] = 'a';
+                console.log(this.operands);
+                this.numString = '';
+                //this.currentOperand = 0;
+                this.updateScreen(this.operands[0]);
+            } else {
+                this.numString = '';
+                this.currentOperand++;
+                console.log(this.currentOperand);
+            }
+        }
+    },
+
+    updateScreen(num) {
+        document.querySelector('.screen').textContent = num;
+    },
+    resetOperands() {
+        this.operands = ['a', 'a'];
+        this.currentOperand = 0;
     }
+
+
 }
   
   
