1b1781e9bed5e9100ee953367effe264228fc931
Sam Sarsten <sam.sarsten@gmail.com>
2022-09-04 16:31:40 -0700

    Changing handling of numbers again

--- script.js
+++ script.js
@@ -1,30 +1,33 @@
 const calculator = {
     keyPress: 'a',
-    operands: ['a', 'a'],
-    currentOperand: 0,
+    currentNum: 'a',
+    lastNum: 'a',  //Necessary?
+    //operands = [0,0],
+    //currentOperand: 0,
     numString: '',
+    operator: '',
     //intString: '',
     //decString: '',
     //float: false,
-    negative: false,
-    screen: '',
+    //negative: false,
+    //screen: '',
 
-    getOperand(index) {
-        return this.operands[index];
-    },
-    getCurrentOperand() {
-        return this.operands[this.currentOperand];
-    },
-    getFloat() {
-        return this.float;
-    },
-    getNumString() {
-        if (this.float) {
-            return Number(this.intString) + "." + Number(this.decString);
-        } else {
-            return Number(this.intString)
-        }
-    },
+    // getOperand(index) {
+    //     return this.operands[index];
+    // },
+    // getCurrentOperand() {
+    //     return this.operands[this.currentOperand];
+    // },
+    // getFloat() {
+    //     return this.float;
+    // },
+    // getNumString() {
+    //     if (this.float) {
+    //         return Number(this.intString) + "." + Number(this.decString);
+    //     } else {
+    //         return Number(this.intString)
+    //     }
+    // },
 
     setKeyPress(key) {
         this.keyPress = key;
@@ -49,23 +52,54 @@ const calculator = {
                 this.numString += kp.toString();
                 //this.screen = this.numString;
                 this.updateScreen(this.numString);
+                this.currentNum = Number(this.numString);
+                console.log("currentNum: " + this.currentNum);
+                console.log("numString: " + this.numString);
             }
-        } else if (kp === "+") {
-            calculator.addition();
-        } else if (kp === "-") {
-            calculator.subtraction();
-        } else if (kp === "*") {
-            calculator.multiplication();
-        } else if (kp === "÷") {
-            calculator.division();
-        } else if (kp === "%") {
-            calculator.percent();
+        } else if (kp === "+" || kp === "-" || kp === "*" || kp === "/") {
+            this.handleOperator(kp);    
+        
+        // else if (kp === "+") {
+        //     calculator.addition();
+        // } else if (kp === "-") {
+        //     calculator.subtraction();
+        // } else if (kp === "*") {
+        //     calculator.multiplication();
+        // } else if (kp === "÷") {
+        //     calculator.division();
+        // } else if (kp === "%") {
+        //     calculator.percent();
+        // } 
         } else if (kp === "B") {
             calculator.backspace();
         } else if (kp === "C") {
             calculator.clear();
         } else if (kp === "=") {
             calculator.evaluate();
+        }
+    },
+
+    handleOperator(kp) {
+        // operator, currentNum, and lastNum assigned and ready for next operator
+        if (this.lastNum !== 'a') {
+            // So, evaluate
+            this.evaluate(this.operator);
+        }
+
+        // Only one number assigned
+        if (this.currentNum !== 'a') {
+            // No operator assigned (Just got done evaluating or just one number so far)
+            // OR, operator assigned but different one was pressed
+            // Either way, just assign this one
+            if (this.operator === '' || this.operator !== kp) {
+                this.operator = kp;
+            }
+        } 
+
+        // No numbers assigned, don't assign operator
+        // Unless you hit 'minus', then do +/- operation
+        if (kp === '-') {
+            this.numString = (this.numString === '-') ? '' : '-';
         }
     },
 
@@ -76,6 +110,28 @@ const calculator = {
             console.log(this.operands);
             if (this.currentOperand === 1) {
                 this.operands[0] = this.operands[0] + this.operands[1];
+                this.operands[1] = 'a';
+                console.log(this.operands);
+                this.numString = '';
+                //this.currentOperand = 0;
+                this.updateScreen(this.operands[0]);
+            } else {
+                this.numString = '';
+                this.currentOperand++;
+                console.log(this.currentOperand);
+            }
+        }
+    },
+
+    subtraction() {
+        if (this.numString.length === 0) { // Allows for negatives
+            this.numString = "-";
+        } else {
+            this.operator = "-";
+            this.operands[this.currentOperand] = Number(this.numString);
+            console.log(this.operands);
+            if (this.currentOperand === 1) {
+                this.operands[0] = this.operands[0] - this.operands[1];
                 this.operands[1] = 'a';
                 console.log(this.operands);
                 this.numString = '';
