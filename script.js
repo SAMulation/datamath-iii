de4fa1d4895d6ebbfbb37d06092f2ec84edc8b55
Sam Sarsten <sam.sarsten@gmail.com>
2022-09-11 14:50:39 -0700

    I like this stopping point for the gui

--- script.js
+++ script.js
@@ -460,5 +460,15 @@ class Calculator {
 
 }
 
+// To open the help for mobile
+helps = document.querySelectorAll('.helpLink');
+helps.forEach(help => {
+    // was click
+    help.addEventListener('click', event => {
+        document.querySelector('.page-sidebar').classList.toggle("visible");
+        document.querySelector('.page-subheader').classList.toggle("hide");
+    });
+})
+
 const calculator1 = new Calculator(document.querySelector('.calc1'));
 // const calculator2 = new Calculator(document.querySelector('.calc2'));
